84eca190aff82047bd2fb57e1d81c1c41eeca373
Adan Baca <adanbaca@arizona.edu>
2024-04-08 16:59:17 -0700

    Setting up stuff

---
+++ src/Parser.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Parser {
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        while (true) {
+
+        }
+    }
+}
